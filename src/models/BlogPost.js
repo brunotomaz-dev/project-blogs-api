4455eebb1c9d7bd113ff2a87cd97de0a8a5afc9e
brunotomaz-dev <tomaz.bruno@gmail.com>
2022-10-26 13:22:03 -0300

    corrigido erro de digitação do underscored

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,5 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', {
+    id: DataTypes.INTEGER,
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: DataTypes.INTEGER,
